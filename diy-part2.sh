bb1987d28e01d97440dbfd9f3076e5718fc9aa57
abt12680 <50388398+abt12680@users.noreply.github.com>
2020-05-25 17:11:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 添加主题&调用diy目录下的文件，包括zzz-default-settings文件
-cd openwrt
+#cd openwrt
 git clone https://github.com/jerrykuku/luci-theme-argon -b 19.07_stable ./package/feeds/luci/themes
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git ./package/feeds/luci/themes
 git clone https://github.com/rosywrt/luci-theme-rosy.git ./package/feeds/luci/themes
